b0ab8e97bd99993d6f316df5618a2ea117f85c8b
MathewLyricist <102455364+MathewLyricist@users.noreply.github.com>
2025-01-05 21:20:50 +0600

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -13,7 +13,6 @@ from django.urls import reverse_lazy, reverse
 from .forms import CommentForm, PostForm, UserProfileForm
 from .models import Post, Category, Comment
 
-
 class PostListView(ListView):
     model = Post
     paginate_by = 10
