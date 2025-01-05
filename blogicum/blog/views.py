696bc183b7f5ad920ed4a61ba0d55ce66c0d1f94
MathewLyricist <102455364+MathewLyricist@users.noreply.github.com>
2025-01-05 21:06:07 +0600

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -81,17 +81,221 @@ class RedirectionProfileMixin:
 
 class PostMixin:
     """Базовый миксин для постов."""
-
     model = Post
 
 
 class PostFormMixin(PostMixin):
     """Миксин для постов с формой."""
-
     form_class = PostForm
 
 
 class PostListMixin(PostMixin):
     """Миксин для страниц со списком постов и пагинацией."""
-
     paginate_by = PAGINATE_COUNT
+
+
+class PostCreateMixin:
+    """Базовый миксин для создания и изменения постов."""
+    template_name = 'blog/create.html'
+
+
+class PostIdCreateMixin(PostCreateMixin):
+    """Расширенный миксин для создания и изменения постов."""
+    pk_url_kwarg = 'post_id'
+
+
+class PostListView(PostListMixin, ListView):
+    """CBV главной страницы. Выводит список постов."""
+    template_name = 'blog/index.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return get_general_posts_filter()
+
+
+class PostDetailView(PostMixin, DetailView):
+    """CBV подробная страница поста с комментариями к нему."""
+    template_name = 'blog/detail.html'
+    pk_url_kwarg = 'post_id'
+
+    # def dispatch(self, request, *args, **kwargs) -> HttpResponse:
+    #     """
+    #     Условия фильтрации, если пользователь не является автором поста:
+    #     - пост разрешён к публикации;
+    #     - категория разрешена к публикации;
+    #     - текущее время больше времени публикации.
+    #     """
+    #     self.object = self.get_object()
+    #     if self.object.author != self.request.user and (
+    #         self.object.is_published is False
+    #         or self.object.category.is_published is False
+    #         or self.object.pub_date > timezone.now()
+    #     ):
+    #         raise Http404
+    #     return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().select_related(
+            'author',
+            'location',
+            'category',
+        )
+
+    def get_object(self, queryset=None):
+        """
+        Условия фильтрации, если пользователь не является автором поста:
+        - пост разрешён к публикации;
+        - категория разрешена к публикации;
+        - текущее время больше времени публикации.
+        """
+        post = super().get_object(queryset)
+        if post.author != self.request.user and (
+            post.is_published is False
+            or post.category.is_published is False
+            or post.pub_date > timezone.now()
+        ):
+            raise Http404
+        return post
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        context['comments'] = self.object.comments.select_related(
+            'author'
+        )
+        return context
+
+
+class CategoryListView(PostListMixin, ListView):
+    """CBV страница категории. Выводит список постов."""
+    template_name = 'blog/category.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return get_general_posts_filter().filter(
+            category__slug=self.kwargs['category_slug'],
+        )
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['category'] = get_object_or_404(
+            Category,
+            is_published=True,
+            slug=self.kwargs['category_slug'],
+        )
+        return context
+
+
+class PostCreateView(
+    LoginRequiredMixin,
+    PostFormMixin,
+    PostCreateMixin,
+    ValidationMixin,
+    RedirectionProfileMixin,
+    CreateView,
+):
+    """CBV страница создания поста."""
+    pass
+
+
+class PostUpdateView(
+    EditContentMixin,
+    PostFormMixin,
+    PostIdCreateMixin,
+    ValidationMixin,
+    RedirectionPostMixin,
+    UpdateView,
+):
+    """CBV страница редактирования поста."""
+    pass
+
+
+class PostDeleteView(
+    EditContentMixin,
+    PostMixin,
+    PostIdCreateMixin,
+    RedirectionProfileMixin,
+    DeleteView,
+):
+    """CBV страница удаления поста."""
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['form'] = PostForm(instance=self.object)
+        return context
+
+
+class ProfilePostListView(PostListMixin, ListView):
+    """CBV страницы пользователя."""
+    template_name = 'blog/profile.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        self.author = get_object_or_404(
+            User,
+            username=self.kwargs['username']
+        )
+        return Post.objects.select_related(
+            'author',
+            'location',
+            'category',
+        ).filter(
+            author=self.author
+        ).annotate(
+            comment_count=Count('comments')
+        ).order_by('-pub_date')
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['profile'] = self.author
+        return context
+
+
+class EditProfileUpdateView(
+    LoginRequiredMixin,
+    RedirectionProfileMixin,
+    UpdateView,
+):
+    """CBV страница редактирования профиля."""
+    model = User
+    template_name = 'blog/user.html'
+    fields = (
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class CommentMixin(RedirectionPostMixin):
+    """Миксин для комментариев."""
+    model = Comment
+    template_name = 'blog/comment.html'
+    pk_url_kwarg = 'comment_id'
+
+
+class CommentFormMixin(CommentMixin):
+    """Миксин для комментариев с формой."""
+    form_class = CommentForm
+
+
+class CommentCreateView(LoginRequiredMixin, CommentFormMixin, CreateView):
+    """CBV создания комментария"""
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(
+            get_general_posts_filter(),
+            pk=self.kwargs['post_id']
+        )
+        return super().form_valid(form)
+
+
+class CommentUpdateView(EditContentMixin, CommentFormMixin, UpdateView):
+    """CBV страница редактирования комментария."""
+    pass
+
+
+class CommentDeleteView(EditContentMixin, CommentMixin, DeleteView):
+    """CBV страница удаления комментария."""
+    pass
