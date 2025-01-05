294cc13d2e057d3c08c7f7ec12c01b97e2911eda
MathewLyricist <102455364+MathewLyricist@users.noreply.github.com>
2025-01-05 21:10:31 +0600

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,19 +1,16 @@
-from typing import Any, Dict
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count
-from django.db.models.query import QuerySet
-from django.http import Http404
 from django.http.response import HttpResponse
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils import timezone
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 
-from .forms import CommentForm, PostForm
-from .models import Category, Comment, Post, User
+from .forms import PostForm
+from .models import Post
 
 PAGINATE_COUNT: int = 10
 
